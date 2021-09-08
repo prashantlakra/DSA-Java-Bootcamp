9de1d2eb189de7da0bffa3100e56e7e700484f99
Prashant <prashant.lakra@outlook.com>
2021-09-08 08:48:16 +0530

    session code added

---
+++ 02-First-Java/Code/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 258;
+        byte b = (byte) (a);
+        int mod1 = 258;
+        int mod2 = 256;
+        System.out.println(mod1 % mod2);
+        System.out.println(b);
+    }
+}
