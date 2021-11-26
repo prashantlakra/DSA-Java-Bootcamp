34c6a3a477d944b6afdb0c60dc6a16d000d32d57
Prashant <prashant.lakra@outlook.com>
2021-11-26 22:32:49 +0530

    assignments added

---
+++ 03-Conditionals-Loops/Assignments/01-Basic-Java-Programs/src/com/company/AreaOfCircle.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the radius: ");
+        float radius = input.nextFloat();
+
+        double aoc = 3.14 * radius * radius;
+        System.out.println("The area of circle is: " +aoc);
+    }
+}
